9d08718b62c5084d3e6c719bf4ac3d237bc3d0b4
Gabrie Balog <gabriel.balog@bradootech.com>
2017-09-26 11:44:00 -0300

    Update localizacao.sh

--- localizacao.sh
+++ localizacao.sh
@@ -4,4 +4,4 @@ echo $ODOO_HOME
 mkdir -p $ODOO_HOME/custom/localizacao
 git clone https://github.com/bradootech/trustcode-addons $ODOO_HOME/localizacao/trustcode-addons
 git clone https://github.com/BradooTech/odoo-brasil $ODOO_HOME/localizacao/odoo-brasil
-sudo chown -R odoo:odoo /odoo/localizacao
+sudo chown -R odoo:odoo /odoo/custom/localizacao
